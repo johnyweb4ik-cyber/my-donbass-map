a0e9cb70e02e32bdd27bf57272136c7aaeb591f5
johnyweb4ik-cyber <johnyweb4ik@gmail.com>
2026-04-08 01:07:58 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// КОНФИГУРАЦИЯ КАРТЫ
+const MAP_CONFIG = {
+    center: [38.0, 48.5],   // [долгота, широта] Донбасс
+    zoom: 8,
+    controls: {
+        zoom: { right: 15, top: 100 }
+    }
+};
