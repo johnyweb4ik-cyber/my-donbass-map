d78b013114fd918d23760afcac56149a9e262817
johnyweb4ik-cyber <johnyweb4ik@gmail.com>
2026-04-08 12:12:51 +0700

    Update map.js

--- js/map.js
+++ js/map.js
@@ -7,9 +7,6 @@ async function initMap() {
     // Импортируем базовые компоненты
     const { YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer } = ymaps3;
     
-    // Импортируем кнопки масштаба из официального пакета controls
-    const { YMapZoomControl } = await ymaps3.import('@yandex/ymaps3-controls@0.0.1');
-
     // Создаём карту
     const map = new YMap(
         document.getElementById('map'),
@@ -26,10 +23,6 @@ async function initMap() {
     map.addChild(new YMapDefaultSchemeLayer());
     map.addChild(new YMapDefaultFeaturesLayer({ zIndex: 1800 }));
     
-    // Добавляем кнопки масштаба (прямо на карту, без дополнительных контейнеров)
-    map.addChild(new YMapZoomControl({ 
-        position: { right: 15, top: 100 } 
-    }));
 
     // Сохраняем карту в глобальную переменную
     window.myMap = map;
