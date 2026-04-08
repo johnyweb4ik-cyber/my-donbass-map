083563bdbd74e636f8d6b580260f142a84cb307e
johnyweb4ik-cyber <johnyweb4ik@gmail.com>
2026-04-08 12:17:13 +0700

    Update markers.js

--- js/markers.js
+++ js/markers.js
@@ -1,8 +1,3 @@
-// МЕТКИ НА КАРТЕ
-// Сюда будете добавлять свои точки
-// Пример:
-// addMarker([38.0, 48.5], "Донецк", "Крупный город");
-
 async function addMarker(coords, title, description) {
     await ymaps3.ready;
     const { YMapPlacemark } = ymaps3;
@@ -16,7 +11,3 @@ async function addMarker(coords, title, description) {
         window.myMap.addChild(marker);
     }
 }
-
-// Пример добавления тестовой метки (раскомментируйте, когда будете готовы)
-// addMarker([37.8, 48.0], "Донецк", "Столица ДНР");
-// addMarker([39.3, 48.5], "Луганск", "Столица ЛНР");
